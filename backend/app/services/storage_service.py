59ac4d41b40a35d39aff4fa1c8f43e2d608802d6
Peter Gatitu <petergatitu61@gmail.com>
2026-01-06 08:53:34 +0300

    Add storage service updates and docker compose changes

--- backend/app/services/storage_service.py
+++ backend/app/services/storage_service.py
@@ -14,7 +14,7 @@ class StorageService:
             "s3",
             endpoint_url=settings.S3_ENDPOINT,
             aws_access_key_id=settings.S3_ACCESS_KEY,
-            aws_secret_key_id=settings.S3_SECRET_KEY,
+            aws_secret_access_key=settings.S3_SECRET_KEY,
             use_ssl=settings.S3_USE_SSL,
             region_name=settings.S3_REGION
         )
